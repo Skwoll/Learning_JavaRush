e5d40cf816e6876fa319d34ad1956312eb686c6c
Skwoll <air_mib@mail.ru>
2017-12-29 15:58:31 +0400

    Add New Task

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
@@ -0,0 +1,86 @@
+package com.javarush.task.task03.task0307;
+
+/* 
+Привет Starcraft!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Zerg z1 = new Zerg()
+                ,z2 = new Zerg()
+                ,z3 = new Zerg()
+                ,z4 = new Zerg()
+                ,z5 = new Zerg()
+                ,z6 = new Zerg()
+                ,z7 = new Zerg()
+                ,z8 = new Zerg()
+                ,z9 = new Zerg()
+                ,z  = new Zerg();
+        Protoss p1 = new Protoss()
+               ,p2 = new Protoss()
+                ,p3 = new Protoss()
+                ,p4 = new Protoss()
+                ,p5 = new Protoss();
+        Terran t0 = new Terran()
+                ,t1 = new Terran()
+                ,t2 = new Terran()
+                ,t3 = new Terran()
+                ,t4 = new Terran()
+                ,t5 = new Terran()
+                ,t6 = new Terran()
+                ,t7 = new Terran()
+                ,t8 = new Terran()
+                ,t9 = new Terran()
+                ,t10 = new Terran()
+                ,t11 = new Terran();
+
+
+        z1.name = "z1";
+        z2.name = "z2";
+        z3.name = "z3";
+        z4.name = "z4";
+        z5.name = "z5";
+        z6.name = "z6";
+        z7.name = "z7";
+        z8.name = "z8";
+        z9.name = "z9";
+        z.name = "z";
+        p1.name = "p1";
+        p2.name = "p2";
+        p3.name = "p3";
+        p4.name = "p4";
+        p5.name = "p5";
+        t0.name = "t0";
+        t1.name = "t1";
+        t2.name = "t2";
+        t3.name = "t3";
+        t4.name = "t4";
+        t5.name = "t5";
+        t6.name = "t6";
+        t7.name = "t7";
+        t8.name = "t8";
+        t9.name = "t9";
+        t10.name = "t10";
+        t11.name = "t11";
+
+
+
+
+
+
+
+    }
+
+    public static class Zerg {
+        public String name;
+    }
+
+    public static class Protoss {
+        public String name;
+    }
+
+    public static class Terran {
+        public String name;
+    }
+}
