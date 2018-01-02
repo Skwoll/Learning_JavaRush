942fdfca16e568a977c3e83c9c9c8f456e1c2dbd
skwoll <air_mib@mail.ru>
2018-01-02 11:13:19 +0400

    Add new task

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i=1;i<=10;i++)
+        {
+            for (int p=1;p<=10;p++)
+                System.out.print(i*p+" ");
+        System.out.println("");
+        }
+    }
+}
