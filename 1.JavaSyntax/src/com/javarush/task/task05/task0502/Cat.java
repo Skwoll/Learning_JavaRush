decc516e5b1ae5f76a6e26df6f14ec6514c4e727
Skwoll <air_mib@mail.ru>
2018-02-19 18:02:19 +0400

    Add New Task

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0502;
+
+/* 
+Реализовать метод fight
+*/
+
+public class Cat {
+    public String name;
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        //напишите тут ваш код
+
+        if (this.age+this.strength+this.weight > anotherCat.weight+anotherCat.age+anotherCat.strength) return true;
+        else return false;
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
