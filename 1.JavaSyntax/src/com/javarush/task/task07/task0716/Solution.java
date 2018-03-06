3af71285b87bf9ea456c01f46100107ab7473f8f
Skwoll <air_mib@mail.ru>
2018-03-06 17:10:08 +0400

    Add New Task

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task07.task0716;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Р или Л
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. Метод fix должен удалять из списка строк все слова, содержащие букву «р». Исключение: слова содержащие и букву «р» и букву «л» - их нужно оставить.
+3. Метод fix должен удваивать слова, содержащие букву «л» (добавлять еще один элемент с этим словом). Исключение: слова содержащие и букву «л» и букву «р» - их не нужно удваивать.
+4. Метод fix не должен ничего делать со словами, содержащими и букву «л» и букву «р».
+5. Метод fix не должен ничего делать со словами, которые не содержат ни букву «л», ни букву «р».
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("роза"); //0
+        list.add("лоза"); //1
+        list.add("лира"); //2
+        list = fix(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> list) {
+        //напишите тут ваш код
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String s:
+             list) {
+            if ((s.contains("л") && s.contains("р")) || (!s.contains("л") && !s.contains("р"))) result.add(s);
+            if (s.contains("л")&&!s.contains("р")) {
+                result.add(s);
+                result.add(s);
+            }
+        }
+        return result;
+    }
+}
