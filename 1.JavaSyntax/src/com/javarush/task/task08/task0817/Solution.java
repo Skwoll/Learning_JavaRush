9c8030f833c4d2604ede150e4dd9a61e899938f9
skwoll <air_mib@mail.ru>
2018-05-24 21:05:29 +0400

    Add new task

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -0,0 +1,62 @@
+package com.javarush.task.task08.task0817;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/* 
+Нам повторы не нужны
+Создать словарь (Map<String, String>) занести в него десять записей по принципу "фамилия" - "имя".
+Удалить людей, имеющих одинаковые имена.
+
+
+Требования:
+1. Программа не должна выводить текст на экран.
+2. Программа не должна считывать значения с клавиатуры.
+3. Класс Solution должен содержать только четыре метода.
+4. Метод createMap() должен создавать и возвращать словарь HashMap с типом элементов String, String состоящих из 10 записей.
+5. Метод removeTheFirstNameDuplicates() должен удалять из словаря всех людей, имеющие одинаковые имена.
+6. Метод removeTheFirstNameDuplicates() должен вызывать метод removeItemFromMapByValue().
+*/
+
+public class Solution {
+    public static HashMap<String, String> createMap() {
+        //напишите тут ваш код
+        HashMap<String, String> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put("LN" + i, "FN" + (i % 7));
+        }
+        return map;
+
+    }
+
+    public static void removeTheFirstNameDuplicates(HashMap<String, String> map) {
+        //напишите тут ваш код
+        Iterator<HashMap.Entry<String,String>> i = map.entrySet().iterator();
+
+
+        while (i.hasNext()) {
+            HashMap.Entry curr = i.next();
+            removeItemFromMapByValue(map, curr.getValue().toString());
+        }
+
+
+//        map.forEach((s, s2) -> removeItemFromMapByValue(map, s2));
+
+    }
+
+    public static void removeItemFromMapByValue(HashMap<String, String> map, String value) {
+        HashMap<String, String> copy = new HashMap<String, String>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            if (pair.getValue().equals(value))
+                map.remove(pair.getKey());
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = createMap();
+        removeTheFirstNameDuplicates(map);
+        map.forEach((s, s2) -> System.out.println(s+" - "+s2));
+
+    }
+}
