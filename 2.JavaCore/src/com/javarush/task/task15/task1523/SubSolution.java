7f1ed345328f996ab1cb4f5cd93185a8dcfcc8a0
skwoll <air_mib@mail.ru>
2018-08-23 07:23:12 +0400

    Add new task

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+import java.security.PublicKey;
+
+public class SubSolution extends Solution {
+    protected SubSolution(int i) {
+        super(i);
+    }
+
+    SubSolution(char c) {
+        super(c);
+    }
+    public SubSolution(){
+        super();
+    }
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME = "/Users/pavel/Documents/test/test3.txt"/* add your path to source file here*/;
+}
