c668239befe96e319e99d1519dda5724d9fc9490
Skwoll <air_mib@mail.ru>
2019-01-11 17:30:30 +0400

    Add New Task

--- 2.JavaCore/src/com/javarush/task/task19/task1918/HTMLParser.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/HTMLParser.java
@@ -2,83 +2,33 @@ package com.javarush.task.task19.task1918;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HTMLParser {
 
 
 
     public static List<String> ParseHTMLTag (String tag,String file){
-        String openTag = "<"+tag;
-        String closeTag = "</"+tag+">";
-        List<Integer> openTags = new ArrayList<Integer>();
-        List<Integer> closeTags = new ArrayList<Integer>();
-        int lastOpenTagIndex = 0;
-        int lastCloseTagIndex = 0;
-
-        while (lastCloseTagIndex >=0 && lastOpenTagIndex>=0)
-        {
-            lastOpenTagIndex = lastOpenTagIndex == -1 ? -1 : file.indexOf(openTag,lastOpenTagIndex);
-            lastCloseTagIndex = lastCloseTagIndex == -1 ? -1 : file.indexOf(closeTag,lastCloseTagIndex);
-            if (lastOpenTagIndex != -1){
-                openTags.add(lastOpenTagIndex);
-                lastOpenTagIndex++;
-            }
-            if (lastCloseTagIndex != -1){
-                closeTags.add(lastCloseTagIndex);
-                lastCloseTagIndex++;
-            }
-        }
-
-        List<Pair> pairs = new ArrayList<>();
-
-        for (int i = 0; i < openTags.size() ; i++) {
-            int curClose = 0;
-            int curOpen = openTags.get(i);
-
-            for (int j = 0; j < closeTags.size(); j++) {
-                if (closeTags.get(j)> curOpen){
-                    curClose = j;
-                    break;
-                }
-            }
-
-            int offOpen = i+1 < openTags.size() ? 1 : 0;
-            int offClose = 0;
-            int openCnt = 1;
-            int closeCnt = 1;
-
-            while(true){
-
-                if (openCnt > openTags.size() || closeCnt > closeTags.size()) {
-                    break;
-                }
-
-                if ((openTags.get(i+offOpen) > closeTags.get(curClose+offClose) || i+offOpen + 1 >= openTags.size() )
-                        && openCnt == closeCnt){
-                    pairs.add(new Pair(curOpen,closeTags.get(curClose+offClose)+closeTag.length()));
-//                    curClose +=offClose+1;
-                    break;
-                } else{
-                    if (openTags.get(i+offOpen)< closeTags.get(curClose+offClose)){
-                        offOpen += i+offOpen+1 >= openTags.size() ? 0 : 1;
-                        openCnt++;
-                    }
-                    else {
-                        offClose += curClose+offClose+1 >= openTags.size() ? 0 :1;
-                        closeCnt++;
-                    }
-
-                }
-
-            }
-        }
-
+        Stack<Integer> integerStack = new Stack<>();
         List<String> result = new ArrayList<>();
 
-        for (Pair p :
-                pairs) {
-            result.add(file.substring((Integer) p.t, (Integer) p.u));
+        Matcher m = Pattern.compile("<(/?)" + tag + ".*?>").matcher(file);
+        TreeMap<Integer,Integer> map = new TreeMap();
+
+        while (m.find()){
+            if (file.charAt(m.start()+1)== '/'){
+                map.put(integerStack.pop(),m.end());
+
+            }else {
+                integerStack.push(m.start());
+            }
         }
+
+        map.forEach((integer, integer2) -> result.add(file.substring(integer,integer2)));
+
 
         return result;
     }
