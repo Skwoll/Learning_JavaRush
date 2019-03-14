f8bd3404e4ac165df0bd2dac18ec83c602d5d62d
Skwoll <air_mib@mail.ru>
2019-03-14 12:18:03 +0400

    Add New Task

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task34.task3404;
 
-import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,17 +14,21 @@ import java.util.regex.Pattern;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
+
         solution.recurse("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
-        solution.recurse("sin( 2 *( -5 +1.5*4)+28) + cos(4+(8-10*2)+10)", 0); //expected output 0.5 6
+        solution.recurse("tan(2025 ^ 0.5)", 0); //expected output 1 2
+
+//        solution.recurse("sin( 2 *( -5 +1.5*4)+28) + cos(4+(8-10*2)+10)", 0); //expected output 0.5 6
 
     }
 
     public void recurse(final String expression, int countOperation) {
         //implement
-        if (countOperation != 0){
-            System.out.println(expression + " "+countOperation);
-            return;
-        }
+//        if (countOperation != 0){
+//            NumberFormat format = new DecimalFormat("#.##");
+//            System.out.printf("%s %d",format.format(expression) ,countOperation);
+//            return;
+//        }
 
         Matcher m = Pattern.compile("(sin|cos|tan|\\^|\\/|\\*|\\-|\\+){1}?").matcher(expression);
         while(m.find()){
@@ -97,7 +102,8 @@ public class Solution {
                             break;
                     }
                     result = new BigDecimal(result).setScale(4, RoundingMode.HALF_UP).doubleValue();
-                    tmpExp.replace(0,tmpExp.length(),m.replaceFirst(String.valueOf(result)));
+
+                    tmpExp.replace(0,tmpExp.length(),m.replaceFirst(String.format("%.2G",result)));
                 }
                 m = Pattern.compile("([-+])\\(((\\1)\\d+\\.?\\d*\\))").matcher(tmpExp);
                 while (m.find()){
@@ -114,6 +120,8 @@ public class Solution {
                 lastindex = lastindex == tmpExp.length() ? 0:start;
             }
         }
+
+
         if (countOperation > 0) {
             recurse(tmpExp.toString(),countOperation);
         }
