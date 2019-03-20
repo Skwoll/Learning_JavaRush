c8028ad100cee3cafc3eb2929facf3d9e6fb6af9
Skwoll <air_mib@mail.ru>
2019-03-20 17:58:04 +0400

    Add New Task

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -18,19 +18,35 @@ public class Solution {
         Solution solution = new Solution();
 
 
-//        Matcher m = Pattern.compile("-?\\d+\\.?\\d*\\*-?\\d+\\.?\\d*").matcher("355+45*4");
-//        m.find();
-//        System.out.println(m.replaceFirst("a"));
-//        System.out.println(m.replaceFirst("aa"));
-//        System.out.println(m.replaceFirst("aaa"));
-//        solution.recurse("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
-//        solution.recurse("tan(2025 ^ 0.5)", 0); //expected output 1 2
-//
-//        solution.recurse("-(-22+22*2) ", 0); //expected output -22 4
-//        solution.recurse("0+0.304", 0); //expected output 0.3 1
-//        solution.recurse("-2^(-2)", 0); //expected output -0.25 3
-//        solution.recurse("tan(44+sin(89-cos(180)^2))", 0); //expected output 1 6
+
+        solution.recurse("sin(2*(-5+1.5*4)+28)", 0); //expected output 0.5 6
+        solution.recurse("tan(45)", 0); //expected output 1 1
+        solution.recurse("tan(-45)", 0); //expected output -1 1
+        solution.recurse("0.305", 0); //expected output 0.3 0
+        solution.recurse("0.3051", 0); //expected output 0.31 0
+        solution.recurse("(0.3051)", 0); //expected output 0.31 0
+        solution.recurse("1+(1+(1+1)*(1+1))*(1+1)+1 ", 0); //expected output 12 8
+        solution.recurse("tan(44+sin(89-cos(180)^2))", 0); //expected output 1 6
+        solution.recurse("-2+(-2+(-2)-2*(2+2)) ", 0); //expected output -14 8
+        solution.recurse("sin(80+(2+(1+1))*(1+1)+2)", 0); //expected output 1 7
+        solution.recurse("1+4/2/2+2^2+2*2-2^(2-1+1) ", 0); //expected output 6 11
+        solution.recurse("10-2^(2-1+1)", 0); //expected output 6 4
+        solution.recurse("2^10+2^(5+5)", 0); //expected output 2048 4
+        solution.recurse("1.01+(2.02-1+1/0.5*1.02)/0.1+0.25+41.1", 0); //expected output 72.96 8
+        solution.recurse("0.000025+0.000012 ", 0); //expected output 0 1
+        solution.recurse("-2-(-2-1-(-2)-(-2)-(-2-2-(-2)-2)-2-2) ", 0); //expected output -3 16
+        solution.recurse("cos(3 + 19*3)", 0); //expected output 0.5 3
         solution.recurse("2*(589+((2454*0.1548/0.01*(-2+9^2))+((25*123.12+45877*25)+25))-547)", 0); //expected output 8302231.36 14
+        solution.recurse("(-1 + (-2))", 0); //expected output -3 3
+        solution.recurse("-sin(2*(-5+1.5*4)+28) ", 0); //expected output -0.5 7
+        solution.recurse("sin(100)-sin(100)", 0); //expected output 0 3
+
+        solution.recurse("-(-22+22*2) ", 0); //expected output -22 4
+        solution.recurse("-2^(-2)", 0); //expected output -0.25 3
+        solution.recurse("-(-2^(-2))+2+(-(-2^(-2)))", 0); //expected output 2.5 10
+
+        solution.recurse("tan(2025 ^ 0.5)", 0); //expected output 1 2
+        solution.recurse("0+0.304", 0); //expected output 0.3 1
 
 
 
@@ -40,7 +56,7 @@ public class Solution {
         //implement
 
         NumberFormat format = new DecimalFormat("#.##", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
-        Matcher m = Pattern.compile("(sin|cos|tan|\\^|\\/|\\*|(\\d|\\))\\-|\\+){1}?").matcher(expression);
+        Matcher m = Pattern.compile("(sin|cos|tan|\\^|\\/|\\*|(\\d|\\))\\-|\\+|\\(|\\)){1}?").matcher(expression);
         if (!m.find()){
             System.out.printf("%s %d\n",format.format(Double.parseDouble(expression)) ,countOperation);
             return;
@@ -89,6 +105,7 @@ public class Solution {
                 switch (c) {
                     case '^':
                         result = Math.pow(a,b);
+                        result = a < 0 && b % 2 == 0 && m.start()>0 && subExp.substring(m.start()-1,m.start()+1).matches("\\d+-")   ? -result : result;
                         break;
                     case '/':
                         result = a / b;
@@ -106,6 +123,7 @@ public class Solution {
                 result = new BigDecimal(result).setScale(4, RoundingMode.HALF_UP).doubleValue();
 
                 subExp = m.replaceFirst(String.valueOf(result));
+                m.reset(subExp);
             }
         }
         tmpExp = tmpExp.replace(start+1,lastIndex,subExp);
@@ -128,12 +146,12 @@ public class Solution {
             }
             result = new BigDecimal(result).setScale(4, RoundingMode.HALF_UP).doubleValue();
 
-//                    tmpExp.replace(0,tmpExp.length(),m.replaceFirst(String.format("%.2G",result)));
             tmpExp.replace(0,tmpExp.length(),m.replaceFirst(format.format(result)));
+            m.reset(tmpExp);
         }
         m = Pattern.compile("([-+])\\(((\\1)\\d+\\.?\\d*\\))").matcher(tmpExp);
         while (m.find()){
-            tmpExp.replace(0,tmpExp.length(), m.replaceFirst(m.group().substring(3,m.group().length()-1)));
+            tmpExp.replace(0,tmpExp.length(), m.replaceFirst("+"+m.group().substring(3,m.group().length()-1)));
         }
         m = Pattern.compile("(?<!\\d)\\(([-+]?\\d+\\.?\\d*\\))").matcher(tmpExp);
         while (m.find()){
