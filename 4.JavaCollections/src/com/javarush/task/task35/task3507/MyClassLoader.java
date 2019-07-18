1086b5513d099bf02cbde9b01ca34542f748794b
Skwoll <air_mib@mail.ru>
2019-07-18 22:31:44 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/MyClassLoader.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/MyClassLoader.java
@@ -1,25 +1,92 @@
 package com.javarush.task.task35.task3507;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
 
-public class MyClassLoader extends ClassLoader {
+public class MyClassLoader extends ClassLoader{
     String path;
-    public MyClassLoader(String path,ClassLoader parent) {
-        super(parent);
-        this.path = path;
+    private Map classesHash = new java.util.HashMap();
+    public MyClassLoader(String Path, ClassLoader Parent){
+        super(Parent);
+        path = Path;
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        Class result= findClass(name);
+        if (resolve)
+            resolveClass(result);
+        return result;
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        try {
-            byte[] b = Files.readAllBytes(Paths.get(path));
-            return defineClass(name, b, 0, b.length);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Class result = (Class) classesHash.get(name);
+        if (result != null) {
+            return result;
         }
-        return super.findClass(name);
+
+        File f = findFile(name.replace('.', '/'), ".class");
+
+        if (f == null) {
+            return findSystemClass(name);
+        }
+
+        try {
+            byte[] classBytes= loadFileAsBytes(f);
+            result= defineClass(null, classBytes, 0,
+                    classBytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(
+                    "Cannot load class " + name + ": " + e);
+        } catch (ClassFormatError e) {
+            throw new ClassNotFoundException(
+                    "Format of class file incorrect for class "
+                            + name + " : " + e);
+        }
+        classesHash.put(name,result);
+        return result;
+    }
+
+    @Override
+    protected URL findResource(String name) {
+        File f = findFile(name, "");
+        if (f==null)
+            return null;
+        try {
+            return f.toURL();
+        } catch(java.net.MalformedURLException e) {
+            return null;
+        }
+    }
+
+    private File findFile(String name, String extension){
+        File f;
+        f = new File((new File(path)).getPath()
+                + File.separatorChar
+                + name.replace('/',
+                File.separatorChar)
+                + extension);
+        if (f.exists())
+            return f;
+        return null;
+    }
+
+    public static byte[] loadFileAsBytes(File file) throws IOException{
+        byte[] result = new byte[(int)file.length()];
+        FileInputStream f = new FileInputStream(file);
+        try {
+            f.read(result,0,result.length);
+        } finally {
+            try {
+                f.close();
+            } catch (Exception e) {
+
+            }
+        }
+        return result;
     }
 }
-
--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,6 +1,14 @@
 package com.javarush.task.task35.task3507;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,27 +17,56 @@ ClassLoader - что это такое?
 */
 public class Solution {
     public static void main(String[] args) {
-        Set<? extends Animal> allAnimals = getAllAnimals(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
-        System.out.println(allAnimals);
-        System.out.println(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
+        try {
+            Set<? extends Animal> allAnimals = getAllAnimals(URLDecoder.decode(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8") + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
+            System.out.println(allAnimals);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static Set<? extends Animal> getAllAnimals(String pathToAnimals)
-    {
-
-        Set<Animal> set = new HashSet<>();
-        MyClassLoader cl = new MyClassLoader(ClassLoader.getSystemClassLoader());
-        File[] files;
-             files = (new File(pathToAnimals)).listFiles();//Files.find(Paths.get(pathToAnimals),0,(p, bfa) -> bfa.isRegularFile() && p.endsWith(".class")).map(Path::toFile).collect(Collectors.toList());
-
+    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
+        Set<Animal> result = new HashSet<>();
         try {
-            System.out.println(files[0].getAbsolutePath());
-            cl.loadClass(files[0].getAbsolutePath());
+            File[] files = new File(pathToAnimals).listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".class");
+                }
+            });
+            for (File file:files) {
+                ClassLoader loader = new MyClassLoader(pathToAnimals,Solution.class.getClassLoader());
+//                Class clazz = Class.forName(Solution.class.getPackage().getName() + ".data."+file.getName().replace(".class",""), true, loader);
+                Class clazz = new ClassFromPath().load(file.toPath());
+
+                if (!Animal.class.isAssignableFrom(clazz)) continue;
+
+                boolean hasConstructor = false;
+                for (Constructor c :
+                        clazz.getConstructors()) {
+                    if (Modifier.isPublic(c.getModifiers()) && c.getParameterTypes().length == 0) {
+                        hasConstructor = true;
+                        break;
+                    }
+                }
+                if(!hasConstructor) continue;
+                result.add((Animal)clazz.newInstance());
+
+            }
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-
-
-        return null;
+        return result;
+    }
+    public static class ClassFromPath extends ClassLoader {
+        public Class<?> load(Path path) {
+            try {
+                byte[] b = Files.readAllBytes(path);
+                return defineClass(null, b, 0, b.length); //here main magic
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
