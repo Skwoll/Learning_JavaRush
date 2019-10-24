d8c20f44bcb7c1449db4756dac5e39c99feb9a70
Skwoll <air_mib@mail.ru>
2019-10-24 19:44:06 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.model.Strategy;
+import com.javarush.task.task28.task2810.view.HtmlView;
+import com.javarush.task.task28.task2810.view.View;
 
 /**
  * Aggregator (1)
@@ -36,6 +39,11 @@ import com.javarush.task.task28.task2810.model.Strategy;
  */
 public class Aggregator {
     public static void main(String[] args) {
+        View view = new HtmlView();
+        Model model = new Model(view,new Provider(new HHStrategy()));
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        ((HtmlView) view).userCitySelectEmulationMethod();
 
     }
 }
