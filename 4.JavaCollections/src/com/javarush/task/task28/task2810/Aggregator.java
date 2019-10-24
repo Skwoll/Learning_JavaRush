23591a8b7dec457d9997410b741fba1658b764dc
Skwoll <air_mib@mail.ru>
2019-10-24 14:53:49 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.HHStrategy;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.model.Strategy;
 
@@ -35,8 +36,7 @@ import com.javarush.task.task28.task2810.model.Strategy;
  */
 public class Aggregator {
     public static void main(String[] args) {
-        Provider provider = new Provider(new Strategy() {
-        });
+        Provider provider = new Provider(new HHStrategy());
         Controller controller = new Controller(provider);
         System.out.println(controller.toString());
     }
