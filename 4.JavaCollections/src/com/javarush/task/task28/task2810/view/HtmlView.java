38c5a996fa99bf19dbc2d9f5b1ab768f68ba478c
Skwoll <air_mib@mail.ru>
2019-10-24 20:43:01 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,6 +3,10 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
@@ -32,6 +36,12 @@ public class HtmlView implements View {
     }
 
     private void updateFile(String content){
+        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(filePath))){
+            fileOutputStream.write(content.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
