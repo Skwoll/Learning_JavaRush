7a840be6618a25150bd293b3ee809f5555e41782
Skwoll <air_mib@mail.ru>
2020-01-08 16:50:28 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CurrencyManipulator.java
@@ -1,12 +1,16 @@
 package com.javarush.task.task26.task2613;
 
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CurrencyManipulator {
     private String currencyCode;
-    private Map<Integer, Integer> denominations = new HashMap<>();
+    private Map<Integer, Integer> denominations = new TreeMap<>(Collections.reverseOrder());
 
     public String getCurrencyCode() {
         return currencyCode;
@@ -38,6 +42,33 @@ public class CurrencyManipulator {
         }
 
         return isEmpty;
+    }
+    public boolean isAmountAvailable(int expectedAmount){
+       return expectedAmount <= getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount)throws NotEnoughMoneyException{
+        Map<Integer, Integer> withdraw = new HashMap<>();
+        AtomicInteger ammount = new AtomicInteger(expectedAmount);
+
+        denominations.forEach((k, v) -> {
+            int cnt=0;
+            if (k <= ammount.get()){
+                cnt = ammount.get() / k;
+                if (cnt <= v){
+                    withdraw.put(k,cnt);
+                    ammount.addAndGet(-(k * cnt));
+                }else{
+                    withdraw.put(k,v);
+                    ammount.addAndGet(-(k * v));
+                }
+            }
+        });
+        if (ammount.get() != 0)
+            throw new NotEnoughMoneyException();
+        withdraw.forEach((k, v) -> denominations.put(k,denominations.get(k)-v));
+        return withdraw;
+
     }
 
 }
--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -1,8 +1,34 @@
 package com.javarush.task.task26.task2613.command;
 
-class WithdrawCommand implements Command {
-    @Override
-    public void execute() {
+import com.javarush.task.task26.task2613.ConsoleHelper;
+import com.javarush.task.task26.task2613.CurrencyManipulator;
+import com.javarush.task.task26.task2613.CurrencyManipulatorFactory;
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
 
+class WithdrawCommand implements Command{
+    @Override
+    public void execute() throws InterruptOperationException {
+       String currency = ConsoleHelper.askCurrencyCode();
+       CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currency);
+       int ammount=0;
+
+
+       while (true){
+           ConsoleHelper.writeMessage("Введите необходумую сумму");
+           try {
+               ammount = Integer.parseInt(ConsoleHelper.readString());
+               if (manipulator.isAmountAvailable(ammount)){
+                   manipulator.withdrawAmount(ammount).forEach((k, v) -> ConsoleHelper.writeMessage( k+" - "+v));
+                   break;
+               }else {
+                   ConsoleHelper.writeMessage("Не возможно выдать запрошенную сумму");
+               }
+           } catch (NumberFormatException e) {
+               ConsoleHelper.writeMessage("Введены некоректные данные");
+           } catch (NotEnoughMoneyException e){
+               ConsoleHelper.writeMessage("Не возможно выдать запрошенную сумму");
+           }
+       }
     }
 }
