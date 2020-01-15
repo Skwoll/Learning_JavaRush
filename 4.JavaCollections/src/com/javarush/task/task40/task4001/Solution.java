c0cdd76f9be46e2fc418eb51fc9d42be3aa62d71
Skwoll <air_mib@mail.ru>
2020-01-15 20:39:50 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -3,11 +3,21 @@ package com.javarush.task.task40.task4001;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 /* 
 POST, а не GET
+Исправь ошибки в методе sendPost, чтобы он отправлял POST-запрос с переданными параметрами.
+Примечание: метод main в тестировании не участвует, но чтобы программа корректно работала локально, можешь зайти на сайт http://requestb.in/, создать свой RequestBin и использовать его в main.
+
+
+Требования:
+1. Метод sendPost должен вызвать метод setRequestMethod с параметром "POST".
+2. Метод sendPost должен устанавливать флаг doOutput у соединения в true.
+3. В OutputStream соединения должны быть записаны переданные в метод sendPost параметры.
+4. Метод sendPost должен выводить результат своей работы на экран.
 */
 
 public class Solution {
@@ -19,8 +29,13 @@ public class Solution {
     public void sendPost(URL url, String urlParameters) throws Exception {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        connection.setRequestMethod("GET");
+        connection.setRequestMethod("POST");
         connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        connection.setDoOutput(true);
+        OutputStream outputStream = connection.getOutputStream();
+
+        outputStream.write(urlParameters.getBytes());
+        outputStream.flush();
 
         int responseCode = connection.getResponseCode();
         System.out.println("Response Code: " + responseCode);
