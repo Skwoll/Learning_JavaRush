295abfc5d769c44a3cb516a03336f4459d44b589
Skwoll <air_mib@mail.ru>
2020-02-02 18:28:52 +0400

    Add new task

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -1,18 +1,43 @@
 package com.javarush.task.task34.task3410.controller;
 
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.Model;
 import com.javarush.task.task34.task3410.view.View;
 
-public class Controller {
+public class Controller implements EventListener{
     private View view;
     private Model model;
 
+    //region Constructors
     public Controller() {
         view = new View(this);
         model = new Model();
         view.init();
     }
+    //endregion
 
+    //region Methods
+    @Override
+    public void move(Direction direction) {
+
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void startNextLevel() {
+
+    }
+
+    @Override
+    public void levelCompleted(int level) {
+
+    }
+
+    //endregion
     public static void main(String[] args) {
         Controller controller = new Controller();
 
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task34.task3410.view;
 
+import com.javarush.task.task34.task3410.controller.EventListener;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
@@ -9,6 +10,7 @@ import java.awt.*;
 
 public class Field extends JPanel {
     View view;
+    EventListener eventListener;
 
     //region Methods
     public void paint(Graphics g) {
@@ -18,6 +20,11 @@ public class Field extends JPanel {
 
     }
     //endregion
+
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
+    }
 
     //region Constructors
     public Field(View view) {
