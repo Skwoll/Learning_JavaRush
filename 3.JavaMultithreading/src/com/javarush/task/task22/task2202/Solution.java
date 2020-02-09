ec61b75d92a20e1a62e01fb61337d2fc79a31136
Skwoll <air_mib@mail.ru>
2020-02-09 18:00:48 +0400

    Add new task

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task22.task2202;
+
+/* 
+Найти подстроку
+Метод getPartOfString должен возвращать подстроку начиная с символа после 1-го пробела и до конца слова,
+которое следует после 4-го пробела.
+
+Пример:
+"JavaRush - лучший сервис обучения Java."
+
+Результат:
+"- лучший сервис обучения"
+
+Пример:
+"Амиго и Диего лучшие друзья!"
+
+Результат:
+"и Диего лучшие друзья!"
+
+На некорректные данные бросить исключение TooShortStringException (сделать исключением).
+
+
+Требования:
+1. Класс TooShortStringException должен быть потомком класса RuntimeException.
+2. Метод getPartOfString должен принимать строку в качестве параметра.
+3. В случае, если в метод getPartOfString были переданы некорректные данные, должно возникнуть исключение
+TooShortStringException.
+4. Метод getPartOfString должен возвращать подстроку начиная с символа после 1-го пробела и до конца слова,
+которое следует после 4-го пробела.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+    }
+
+    public static String getPartOfString(String string) {
+        if (string == null) throw new TooShortStringException();
+        String[] result = string.split(" ");
+
+        if (result.length<5)  throw new TooShortStringException();
+
+        return result[1]+' '+result[2]+' '+result[3]+' '+result[4];
+    }
+
+    public static class TooShortStringException  extends RuntimeException {
+    }
+}
