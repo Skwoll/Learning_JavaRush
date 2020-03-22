7bc967a708bed7a6c29f33ec0ca0a08b3d4c1f54
Skwoll <air_mib@mail.ru>
2020-03-22 16:23:37 +0400

    Add new task

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,6 +1,17 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+    private double direction;
+    private double speed;
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
     @Override
     public void draw(Canvas canvas) {
         
@@ -13,5 +24,10 @@ public class Stand extends BaseObject{
 
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
+    }
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 }
