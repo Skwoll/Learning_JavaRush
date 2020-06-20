9bbafbf8859baa4b95fdc561707ec8c3ac293a99
Skwoll <air_mib@mail.ru>
2020-06-20 12:59:22 +0400

    Add new task

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task36.task3608.model;
 
+import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.model.service.UserService;
 import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
 
 public class MainModel implements Model {
     private ModelData modelData = new ModelData();
@@ -13,6 +16,14 @@ public class MainModel implements Model {
 
     @Override
     public void loadUsers() {
-        modelData.setUsers(userService.getUsersBetweenLevels(1,100));
+        List<User> users =userService.getUsersBetweenLevels(1,100);
+        modelData.setUsers(users);
     }
+
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+    }
+
+
 }
